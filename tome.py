ef0bcbc41f22de881340c7312f5b68ebc005a83b
Karl Monaghan <karl_monaghan@mastercard.com>
2018-06-05 21:43:47 +0100

    Moved targeting to tome

--- tome.py
+++ tome.py
@@ -13,6 +13,51 @@ CONFUSE_NUM_TURNS = 10
 FIREBALL_RADIUS = 3
 FIREBALL_DAMAGE = 25
 
+def closest_npc(max_range):
+    #find closest enemy, up to a maximum range, and in the player's FOV
+    closest_enemy = None
+    closest_dist = max_range + 1  #start with (slightly more than) maximum range
+
+    for object in baseclasses.objects:
+        if object.fighter and not object == pc.player and libtcod.map_is_in_fov(baseclasses.fov_map, object.x, object.y):
+            #calculate distance between this object and the player
+            dist = pc.player.distance_to(object)
+            if dist < closest_dist:  #it's closer, so remember it
+                closest_enemy = object
+                closest_dist = dist
+    return closest_enemy
+    
+def target_tile(max_range=None):
+    global key, mouse
+    #return the position of a tile left-clicked in player's FOV (optionally in a range), or (None,None) if right-clicked.
+    while True:
+        #render the screen. this erases the inventory and shows the names of objects under the mouse.
+        libtcod.console_flush()
+        libtcod.sys_check_for_event(libtcod.EVENT_KEY_PRESS | libtcod.EVENT_MOUSE, key, mouse)
+        render_all()
+
+        (x, y) = (mouse.cx, mouse.cy)
+
+        if mouse.rbutton_pressed or key.vk == libtcod.KEY_ESCAPE:
+            return (None, None)  #cancel if the player right-clicked or pressed Escape
+
+        #accept the target if the player clicked in FOV, and in case a range is specified, if it's in that range
+        if (mouse.lbutton_pressed and libtcod.map_is_in_fov(baseclasses.fov_map, x, y) and
+                (max_range is None or pc.player.distance(x, y) <= max_range)):
+            return (x, y)
+
+def target_npc(max_range=None):
+    #returns a clicked npc inside FOV up to a range, or None if right-clicked
+    while True:
+        (x, y) = target_tile(max_range)
+        if x is None:  #player cancelled
+            return None
+
+        #return the first clicked npc, otherwise continue looping
+        for obj in baseclasses.objects:
+            if obj.x == x and obj.y == y and obj.fighter and obj != pc.player:
+                return obj
+
 def cast_heal():
     #heal the player
     if pc.player.fighter.hp == pc.player.fighter.max_hp:
