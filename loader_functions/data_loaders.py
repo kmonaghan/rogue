9a17962c4359bb8e15d6fdde91f4ee5f5e2563ed
Karl Monaghan <karl.monaghan@mastercard.com>
2018-08-01 15:01:45 +0100

    Start on getting loading working

--- loader_functions/data_loaders.py
+++ loader_functions/data_loaders.py
@@ -4,16 +4,12 @@ import shelve
 
 
 def save_game(player, game_map, message_log, game_state):
-    #print "This would save"
-#    with shelve.open('savegame.dat', 'n') as data_file:
-#        data_file['player_index'] = entities.index(player)
-#        data_file['entities'] = entities
-#        data_file['game_map'] = game_map
-#        data_file['message_log'] = message_log
-#        data_file['game_state'] = game_state
+    with shelve.open('savegame.dat', 'n') as data_file:
+        data_file['player_index'] = game_map.entities.index(player)
+        data_file['game_map'] = game_map
+        data_file['message_log'] = message_log
+        data_file['game_state'] = game_state
 
-    #f.close()
-    print ("save_game")
 
 def load_game():
     if not os.path.isfile('savegame.dat'):
@@ -21,11 +17,10 @@ def load_game():
 
     with shelve.open('savegame.dat', 'r') as data_file:
         player_index = data_file['player_index']
-        entities = data_file['entities']
         game_map = data_file['game_map']
         message_log = data_file['message_log']
         game_state = data_file['game_state']
 
     player = entities[player_index]
 
-    return player, entities, game_map, message_log, game_state
+    return player, game_map, message_log, game_state
